3dfd2cacef28f9b012b7d986dbafd7d241f64ba6
olcaytaner <olcay.yildiz@ozyegin.edu.tr>
2026-04-09 22:41:55 +0300

    Added Amr annotation.

--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,9 @@
 <?php
 
+use olcaytaner\Amr\Corpus\AmrConnection;
+use olcaytaner\Amr\Corpus\AmrCorpus;
+use olcaytaner\Amr\Corpus\AmrSentence;
+use olcaytaner\Amr\Corpus\AmrWord;
 use olcaytaner\AnnotatedSentence\AnnotatedCorpus;
 use olcaytaner\AnnotatedSentence\AnnotatedSentence;
 use olcaytaner\AnnotatedSentence\AnnotatedWord;
@@ -29,6 +33,7 @@ use olcaytaner\WordNet\WordNet;
 class DisplayParameter {
     public string $corpusName;
     public AnnotatedCorpus $corpus;
+    public AmrCorpus $amrCorpus;
     public ?TreeBank $treebank;
     public ?TreeBankDrawable $treebankdrawable;
     public string $word;
@@ -37,6 +42,20 @@ class DisplayParameter {
     public string $color;
     public string $field_name;
     public string $layer;
+}
+
+class Rectangle{
+    public int $x;
+    public int $y;
+    public int $width;
+    public int $height;
+
+    public function __construct(int $x, int $y, int $width, int $height){
+        $this->x = $x;
+        $this->y = $y;
+        $this->width = $width;
+        $this->height = $height;
+    }
 }
 
 class Point{
@@ -154,6 +173,16 @@ function matches_ud_tag(AnnotatedWord $currentWord, string $word): bool{
     return $currentWord->getUniversalDependency()->__toString() == $word;
 }
 
+function amr_matches(AmrWord $currentWord, string $word, string $search_type): bool{
+    switch ($search_type) {
+        default:
+        case "full":
+            return str_ends_with($currentWord->getName(), "/" . $word) || $currentWord->getName() == $word;
+        case "contains":
+            return str_contains($currentWord->getName(), $word);
+    }
+}
+
 function english_tree_matches(ParseNode $currentNode, string $word, string $search_type): bool{
     switch ($search_type) {
         default:
@@ -253,6 +282,118 @@ function search_turkish_treebank_for_word(DisplayParameter $parameter): array{
         }
     }
     return $sentences;
+}
+
+function search_amr_for_word(DisplayParameter $parameter): array{
+    $sentences = [];
+    for ($i = 0; $i < $parameter->amrCorpus->sentenceCount(); $i++) {
+        $sentence = $parameter->amrCorpus->getSentence($i);
+        for ($j = 0; $j < $sentence->wordCount(); $j++) {
+            $currentWord = $sentence->getWord($j);
+            if ($currentWord instanceof AmrWord && amr_matches($currentWord, $parameter->word, $parameter->search_type)) {
+                $sentences[] = $sentence;
+                break;
+            }
+        }
+    }
+    return $sentences;
+}
+
+function getBoundingBox(AmrSentence $sentence): Rectangle{
+    $result = new Rectangle(0, 0, 0, 0);
+    $first = true;
+    for ($i = 0; $i < $sentence->wordCount(); $i++) {
+        $word = $sentence->getWord($i);
+        $stringSize = (int) (7.3 * strlen($word->getName()));
+        $x = $word->getPosition()->getX() - $stringSize - 5;
+        $y = $word->getPosition()->getY() - 15;
+        if ($first){
+            $result = new Rectangle($x, $y,  2 * $stringSize + 20, 80);
+            $first = false;
+        } else {
+            if ($x < $result->x){
+                $result->width += $result->x - $x;
+                $result->x = $x;
+            } else {
+                if ($x + 2 * $stringSize + 20 > $result->x + $result->width){
+                    $result->width += $x + 2 * $stringSize + 20 - $result->x - $result->width;
+                }
+            }
+            if ($y < $result->y){
+                $result->height += $result->y - $y;
+                $result->y = $y;
+            } else {
+                if ($y + 80 > $result->y + $result->height){
+                    $result->height += $y + 80 - $result->y - $result->height;
+                }
+            }
+        }
+    }
+    return $result;
+}
+
+function calculatePoints(int $x1, int $y1, int $x2, int $y2, array &$xpoints, array &$ypoints): void{
+    $d = 10;
+    $h = 5;
+    $dx = $x2 - $x1;
+    $dy = $y2 - $y1;
+    $D = sqrt($dx * $dx + $dy * $dy);
+    $xm = $D - $d;
+    $xn = $xm;
+    $ym = $h;
+    $yn = -$h;
+    $sin = $dy / $D;
+    $cos = $dx / $D;
+    $x = $xm * $cos - $ym * $sin + $x1;
+    $ym = $xm * $sin + $ym * $cos + $y1;
+    $xm = $x;
+    $x = $xn * $cos - $yn * $sin + $x1;
+    $yn = $xn * $sin + $yn * $cos + $y1;
+    $xn = $x;
+    $xpoints[1] = $xm;
+    $xpoints[2] = $xn;
+    $ypoints[1] = $ym;
+    $ypoints[2] = $yn;
+}
+
+function create_amr(DisplayParameter $parameter): string{
+    $display = "";
+    $sentences = search_amr_for_word($parameter);
+    foreach ($sentences as $sentence) {
+        $display .= "<h2>" . substr($sentence->getFileName(), strrpos($sentence->getFileName(), "/") + 1) . "</h2>";
+        $box = getBoundingBox($sentence);
+        $display .= "<svg width=\"" . $box->x + $box->width . "\" height=\"" . $box->y + $box->height . "\">\n";
+        for ($j = 0; $j < $sentence->wordCount(); $j++) {
+            $currentWord = $sentence->getWord($j);
+            if ($currentWord instanceof AmrWord){
+                $stringSize = (int) (7.3 * strlen($currentWord->getName()));
+                $display .= "<ellipse rx=\"" . ($stringSize / 2 + 10) . "\" ry=\"" . (20) . "\" cx=\"" . ($currentWord->getPosition()->getX() + 5) . "\" cy=\"" . ($currentWord->getPosition()->getY() + 25) . "\"/>\n";
+                $display .= "<text x=\"" . ($currentWord->getPosition()->getX() - $stringSize / 2 + 5) . "\" y=\"" . ($currentWord->getPosition()->getY() + 20 + 10) . "\">" . $currentWord->getName() . "</text>\n";
+            }
+        }
+        if ($sentence instanceof AmrSentence){
+            for ($j = 0; $j < $sentence->connectionCount(); $j++) {
+                $connection = $sentence->getConnection($j);
+                $from = $connection->getFrom();
+                $to = $connection->getTo();
+                $with  = $connection->getWith();
+                $x1 = $from->getPosition()->getX();
+                $y1 = $from->getPosition()->getY() + 45;
+                $x2 = $to->getPosition()->getX();
+                $y2 = $to->getPosition()->getY() + 5;
+                $display .= "<text x=\"" . (($x1 + $x2) / 2) . "\" y=\"" . (($y1 + $y2) / 2) . "\">" . $with . "</text>\n";
+                $display .= "<line x1=\"" . $x1 . "\" y1=\"" . $y1 . "\" x2=\"" . $x2 . "\" y2=\"" . $y2 . "\"/>\n";
+                $xpoints = [0, 0, 0];
+                $ypoints = [0, 0, 0];
+                $xpoints[0] = $x2;
+                $ypoints[0] = $y2;
+                calculatePoints($x1, $y1, $x2, $y2, $xpoints, $ypoints);
+                $display .= "<polygon points=\"" . $xpoints[0] . "," . $ypoints[0] . " " . $xpoints[1] . "," . $ypoints[1] . " " . $xpoints[2] . "," . $ypoints[2] . "\"/>";
+            }
+        }
+        $display .= "</svg>\n";
+    }
+    return $display;
 }
 
 function create_parse_tree(DisplayParameter $parameter): string{
---
+++ src/turkish-amr.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+<link rel="stylesheet" href="../treestyle.css">
+<head>
+    <meta charset="UTF-8">
+    <title>Turkish Amr Datasets</title>
+</head>
+<body>
+<?php
+
+use olcaytaner\Amr\Corpus\AmrCorpus;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+ini_set('memory_limit', '4096M');
+set_time_limit(0);
+include 'functions.php';
+?>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <table>
+        <tr><td><p>Search type:</p>
+                <input type="radio" id="word" name="search_type" value="full">
+                <label for="word">Full Match</label><br>
+                <input type="radio" id="contains" name="search_type" value="contains" checked="checked">
+                <label for="contains">Contains</label><br>
+            </td>
+            <td><p>Search dataset:</p>
+                <input type="radio" id="gb" name="dataset" value="gb" checked="checked">
+                <label for="gb">gb</label>
+                <input type="radio" id="tourism" name="dataset" value="tourism" checked="checked">
+                <label for="tourism">tourism</label>
+            </td>
+        </tr>
+    </table>
+    <label for="word">Search word:</label>
+    <input type="text" id="word" name="word" size="100" required><br><br>
+    <input type="submit" name="submit_word" value="Amr Annotation">
+</form>
+<?php
+if (isset($_POST['submit_word'])) {
+    $parameter = new DisplayParameter();
+    $parameter->word = $_POST['word'];
+    $parameter->search_type = $_POST['search_type'];
+    $dataset = $_POST['dataset'];
+    switch ($dataset) {
+        case "gb":
+            $parameter->amrCorpus = new AmrCorpus("../Gb/Amr");
+            $parameter->corpusName = "Gb";
+            echo create_amr($parameter);
+            break;
+        case "tourism":
+            $parameter->amrCorpus = new AmrCorpus("../Etstur/Amr");
+            $parameter->corpusName = "Tourism";
+            echo create_amr($parameter);
+            break;
+    }
+}
+?>
+</body>
+</html>
